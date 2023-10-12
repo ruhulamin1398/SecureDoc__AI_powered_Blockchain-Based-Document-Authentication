ea3320c48d87da90a16526ca438c81392be05020
ruhulamin1398 <ruhul.ok@gmail.com>
2023-10-12 20:33:45 +0600

    transaction context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import DocumentVerificationApp from './DocumentVerificationApp'
+import './App.css' 
+import { TransactionContext } from './context/TransactionContext'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const { value } = useContext(TransactionContext)
 
   return (
     <>
  
  
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world! {value}
     </h1>
 
 
