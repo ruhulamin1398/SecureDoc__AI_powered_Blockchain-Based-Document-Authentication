d23b76e12f6cb2f39399f6a623cd8a2863ea23ba
Your Name <you@example.com>
2023-10-20 21:41:41 +0600

    Cheak Document and ChangePassword and Updateded

--- client/src/Component/FullReport/FullReport.jsx
+++ client/src/Component/FullReport/FullReport.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import dp from '../../asset/Image/DP.jpg';
 
 const Load = () => {
     const [data, setData] = useState({});
@@ -29,32 +30,46 @@ const Load = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-7">
-                            <div className="img-container overflow-hidden h-4/5 w-full">
+                            <div className="relative overflow-hidden pb-[60%]">
                                 {data[0].img.data && (
-                                    <Link
-                                        to={`/fullimage/${id}`}
-
-                                    >
-                                        <img
-                                            src={`data:image/png;base64,${arrayBufferToBase64(data[0].img.data)}`}
-                                            alt="Certificate"
-                                            className="w-full transform transition-transform hover:scale-105"
-                                        />
-                                    </Link>
+                                    <a href={`data:application/pdf;base64,${arrayBufferToBase64(data[0].img.data)}`} download="certificate.pdf">
+                                        <object
+                                            data={`data:application/pdf;base64,${arrayBufferToBase64(data[0].img.data)}`}
+                                            type="application/pdf"
+                                            className="w-full h-full absolute top-0 left-0"
+                                        >
+                                            Your browser does not support PDFs.
+                                        </object>
+                                        <Link to={`/fullimage/${id}`} target='_blank'> <div className="absolute bottom-0 right-0 w-16 h-16 border-8 border-dotted border-gray-950 cursor-pointer">
+                                            &nbsp;
+                                        </div></Link>
+                                    </a>
                                 )}
                             </div>
+
                         </div>
 
-                        <div className="col-md-4">
-                            <div className="text-center md:text-left">
-                                <h1 className="text-3xl font-bold mb-4">{data[0].name && data[0].name}</h1>
-                                <h3 className="text-xl mb-4">{data[0].date && data[0].date}</h3>
+                        <div className="col-md-5">
+                            <div className="flex flex-col justify-between h-full">
+                                <div className="bg-blue-200 p-6 rounded-lg shadow-lg">
+                                    <div className="rounded-full overflow-hidden h-16 w-16">
+                                        <img src={dp} alt="Avatar" className="w-full h-full object-cover" />
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className="ml-4">
+                                            <h1 className="text-2xl font-bold text-gray-800 mb-2"> Completed by {data[0].username && data[0].username}</h1>
+                                            <h1 className="text-lg font-bold text-gray-800 mb-1">Document Uploaded {data[0].date && data[0].date}</h1>
+                                            <h3 className="text-lh text-gray-800">
+                                                {data[0].username && data[0].username} is <a href="#" className="theam-color font-bold">Verified</a>. He successfully completed {data[0].CourseName && data[0].CourseName} from {data[0].name && data[0].name}.
+                                                He started the course on {data[0].CourseStart && data[0].CourseStart} and completed on {data[0].CourseEnd && data[0].CourseEnd}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h1 className="text-lg font-bold text-gray-800 mb-1">Course Name: {data[0].CourseName && data[0].CourseName}<br />Completed From {data[0].name && data[0].name}</h1>
                                 <div className="button">
                                     {data[0].img.data && (
-                                        <a
-                                            href={`data:image/png;base64,${arrayBufferToBase64(data[0].img.data)}`}
-                                            download
-                                        >
+                                        <a href={`data:application/pdf;base64,${arrayBufferToBase64(data[0].img.data)}`} download="certificate.pdf">
                                             <button className="bg-theam-color text-white rounded-full py-2 px-4 hover:bg-theam-color">
                                                 Download Certificate
                                             </button>
