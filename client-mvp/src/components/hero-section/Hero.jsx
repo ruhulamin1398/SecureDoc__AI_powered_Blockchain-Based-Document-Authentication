865836e3d8ef8694750399ae21efffffb9340ef9
RK289g <rksaju80@gmail.com>
2024-03-26 23:59:13 +0600

    navbar added

--- client-mvp/src/components/hero-section/Hero.jsx
+++ client-mvp/src/components/hero-section/Hero.jsx
@@ -1,26 +1,70 @@
 import { Button, Input } from "antd";
 import "./Hero.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 const { Search } = Input;
 const Hero = () => {
   const fileInputRef = useRef(null);
+  const [showRegistration, setShowRegistration] = useState(false);
+  const [showSearch, setShowSearch] = useState(true);
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
   };
 
+  const handleLoginClick = () => {
+    setShowRegistration(true);
+    setShowSearch(false);
+  };
+
+  const handleSearchClick = () => {
+    setShowRegistration(false);
+    setShowSearch(true);
+  };
+
   return (
     <div className="hero-wrapper">
+      <div className="hero-inner-wrapper">
       <h3 className="hero-title font-ibm">SecureDoc Universal Verifier</h3>
-      <div className="search-container">
-        <Search
-          placeholder="Enter URL"
-          allowClear
-          enterButton="Verify"
-          size="large"
-          // onSearch={onSearch}
-        />
-      </div>
+      {showSearch && (
+        <div className="search-container">
+          <Search
+            placeholder="Enter URL"
+            allowClear
+            enterButton="Verify"
+            size="large"
+            onClick={handleSearchClick}
+            className="url-search"
+            // onSearch={onSearch}
+          />
+        </div>
+      )}
+      {showRegistration && (
+        <div className="login-container">
+          <div>
+            <Input
+              placeholder="Registration"
+              size="large"
+              className="login-input"
+            />
+          </div>
+
+          <div>
+            <Input.Password
+              placeholder="Password"
+              size="large"
+              className="login-input"
+            />
+          </div>
+          <div>
+            <Button
+              className="login-button"
+              // onClick={HandleLoginClick}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      )}
       <div className="pdf-regi-wrapper">
         <div className="pdf-wrapper">
           <input
@@ -35,22 +79,34 @@ const Hero = () => {
           </p>
         </div>
         <div>
-          <p className="pdf-text"> login via Registration and password</p>
+          {showSearch && (
+            <p onClick={handleLoginClick} className="pdf-text">
+              {" "}
+              login via Registration and password
+            </p>
+          )}
+          {showRegistration && (
+            <p onClick={handleSearchClick} className="pdf-text">
+              {" "}
+              Enter URL
+            </p>
+          )}
         </div>
       </div>
       <div className="credential-wrapper">
-        <p>
+        <p className="credential-text font-ibm">
           You may share your Blockcerts with CHAPI if you have a compatible
           wallet:
         </p>
         <div className="credential-button-wrapper">
-          <Button danger>Credential</Button>
+          <Button danger> Share Credential</Button>
         </div>
-        <p>
+        <p className="credential-note">
           NOTA: the credential remains on the browser and is not stored
           anywhere.
         </p>
       </div>
+    </div>
     </div>
   );
 };
